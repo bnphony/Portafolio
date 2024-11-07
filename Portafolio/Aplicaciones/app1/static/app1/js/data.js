b82291cf7736af2e0b4e6073f5c8422603223c55
Bryan <bryantb479@gmail.com>
2024-11-06 22:59:39 -0500

    toda la informacion importante puesta

---
+++ Portafolio/Aplicaciones/app1/static/app1/js/data.js
@@ -0,0 +1,102 @@
+export const experiencia = [
+    {
+        'cargo': 'Desarrollador Full-Stack',
+        'empresa': 'Liga Deportiva de la Parroquia Mulaló',
+        'fecha_inicio': 'Enero-2024',
+        'fecha_fin': 'Marzo-2024',
+        'actividades': [
+            '- Análisis y diseño del sistema.',
+            '- Toma de requisitos.',
+            '- Creación de Prototipos.',
+            '- Desarrollo de un Sistema para la Gestión de un Campeonato de Fútbol.',
+            '- Implementación del sistema en un Servidor Web.',
+            '- Generación de Reportes.'
+        ],
+    },
+    {
+        'cargo': 'Técnico en Informática',
+        'empresa': 'Sacompu',
+        'fecha_inicio': 'Enero-2023',
+        'fecha_fin': 'Junio-2023',
+        'actividades': [
+            '- Soporte y Mantenimiento de Equipos de Cómputo y Redes.',
+            '- Implementación del cableado de redes informáticas.',
+            '- Gestión estratégica de inventarios.',
+        ],
+    }
+];
+
+export const proyectos = [
+    {
+        'nombre': 'Sistema para la Gestión de un Campeonato de Fútbol',
+        'descripcion': `
+            Campeonato de la Liga Deportiva de la Parroquia Mulaló, 
+            gestionar los equipos, jugadores, categorias, copas, rondas, partidos, etc. Generar los calendarios, los partidos, generar 
+            reportes de los resultados, etc.
+        `,
+        'tecnologias': ['Python', 'Django', 'postgresql', 'HTML-CSS-JavaScript', 'JQuery', 'Ajax', 'Bootstrap'],
+        'imagen': 'app1/img/proyecto_1.png',
+        'url': 'https://campeonato-mulalo.top/',
+        'github': '',
+    },
+    {
+        'nombre': 'Gestionar Ventas',
+        'descripcion': `
+            Aplicación Móvil sobre un Sistema de Venta de Productos.
+        `,
+        'tecnologias': ['Java', 'Android Studio', 'XML'],
+        'imagen': '',
+        'url': '',
+        'github': 'https://github.com/bnphony/Ventas-AS',
+    },
+    {
+        'nombre': 'Cronometro',
+        'descripcion': `
+            Aplicación Móvil para programar eventos y controlar el tiempo para que ocurran.
+        `,
+        'tecnologias': ['Java', 'Android Studio', 'XML'],
+        'imagen': '',
+        'url': '',
+        'github': 'https://github.com/bnphony/Cronometro-AS',
+    },
+    {
+        'nombre': 'Calificar Series',
+        'descripcion': `
+            Sistema para la Valoración de Series/Películas, además de presentar reportes.
+        `,
+        'tecnologias': ['PHP', 'Mysql', 'HTML-CSS-JavaScript', 'D3js'],
+        'imagen': '',
+        'url': '',
+        'github': 'https://github.com/bnphony/Calificar-Series',
+    },
+    {
+        'nombre': 'Suma Mental',
+        'descripcion': `
+            Sistema para ejercitar la resolución de operaciones matemáticas (suma/resta) mentalmente.
+        `,
+        'tecnologias': ['python', 'django', 'jquery', 'd3js', 'HTML-CSS-JavaScript'],
+        'imagen': '',
+        'url': '',
+        'github': 'https://github.com/bnphony/Suma-Mental',
+    },
+    {
+        'nombre': 'Lista Comentada',
+        'descripcion': `
+            Aplicación Web para convertir una lista simple en un ranking ordenado.
+        `,
+        'tecnologias': ['HTML-CSS-JavaScript', 'JQuery'],
+        'imagen': '',
+        'url': '',
+        'github': 'https://github.com/bnphony/ListaComentada',
+    },
+    {
+        'nombre': 'Sistema de Inventario',
+        'descripcion': `
+            Sistema de Inventarios y generación de reportes.
+        `,
+        'tecnologias': ['python', 'django', 'HTML-CSS-JavaScript'],
+        'imagen': '',
+        'url': '',
+        'github': 'https://github.com/bnphony/Sistema-Inventario',
+    },
+];
--- Portafolio/Aplicaciones/app1/static/app1/js/inicio.js
+++ Portafolio/Aplicaciones/app1/static/app1/js/inicio.js
@@ -1,19 +1,127 @@
+import { proyectos, experiencia } from './data.js';
+
 function mostrarJob() {
     // Hide all job elements
     $('.job').hide();
-    
+    console.log("SE EJECUTA");
     // Get the data-id of the selected input
     const id = $(this).data('id');
-    
+    console.log('id: ', id);
     // Show only the selected job if checked
     if ($(this).is(':checked')) {
         $(`#job${id}`).show();
     }
 }
 
-$(function() {
+function graficarProyectos() {
+    let htmlPrincipales = '';
+    let htmlSecundarios = '';
+    proyectos.forEach((item, index) => {
+        if (index < 5) {
+            htmlPrincipales += `
+                <div class="p-principal">
+                    <div class="p-info">
+                        <div class="header">
+                            <h3>${item.nombre}</h3>
+                        </div>
+                        <div class="descripcion">
+                            <p>${item.descripcion}</p>
+                        </div>
+                        <div class="tecnologias">
+                            <ul>
+                                ${item.tecnologias.map(tecno => {
+                                    return '<li>' + tecno + '</li>';
+                                }).join('')}
+                            </ul>
+                            <div class="t-links">
+                                ${item.github !== '' ? '<span><i class="fab fa-github"></i></span>' : ''}
+                                ${item.url !== '' ? '<span><i class="fas fa-external-link-square-alt"></i></span>' : ''}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-img">
+                        <img src="static/${item.imagen !== '' ? item.imagen : 'img/murasa.png'}" alt="Proyecto 1">
+                    </div>
+                </div>       
+            `;
+        } else {
+            htmlSecundarios += `
+                <div class="s-proyecto">
+                <div class="s-icono">
+                    <i class="far fa-folder"></i>
+                </div>
+                <div class="s-info">
+                    <div class="s-header">
+                        <h4>${item.nombre}</h4>
+                        <div>
+                            ${item.github !== '' ? '<span><i class="fab fa-github"></i></span>' : ''}
+                            ${item.url !== '' ? '<span><i class="fas fa-external-link-square-alt"></i></span>' : ''}
+                        </div>
+                    </div>
+                    <div class="s-content">
+                        <p>${item.descripcion}</p>
+                    </div>
+                    <div class="s-lenguages">
+                        <ul>
+                            ${item.tecnologias.map(tecno => {
+                                return '<li>' + tecno + '</li>';
+                            }).join('')}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            `;
+        }
+    });
+
+    $('.p-principales').html(htmlPrincipales);
+    $('.p-secundarios').html(htmlSecundarios);
+}
+
+function graficarExperiencia() {
+    let htmlNAV = '';
+    let htmlJobs = '';
+    experiencia.forEach((item, index) => {
+        htmlNAV += `
+            <label for="idPuesto${index}">${item.cargo}
+                <input id="idPuesto${index}" data-id="${index}" type="radio" name="jobs" ${index === 0 ? 'checked' : ''}>
+            </label>
+        `;
+        htmlJobs += `
+            <div class="job" id="job${index}">
+                <div class="desc-puesto">
+                    <p>${item.cargo}</p>
+                    <p>${item.fecha_inicio} - ${item.fecha_fin}</p>
+                </div>
+                <div class="desc-tareas">
+                    <ul>
+                    ${item.actividades.map((tarea, i) => {
+                        return '<li>' + tarea + '</li>'
+                    }).join('')}
+                    </ul>
+                </div>
+                <div class="desc-tecnologias">
+                    <span>Lenguaje1</span>
+                    <span>Lenguaje2</span>
+                    <span>Lenguaje3</span>
+                    <span>Lenguaje4</span>
+                    <span>Lenguaje5</span>
+                </div>
+            </div>
+        `;
+    });
+    $('.nav-experiencia').html(htmlNAV);
+    $('.descripcion-experiencia').html(htmlJobs);
+    
     // Set up the event listener and trigger it initially
     $('input[name="jobs"]').on('change', mostrarJob);
     // Check initial state of all inputs on page load
     $('input[name="jobs"]:checked').each(mostrarJob);
+}
+
+$(function() {
+
+    graficarExperiencia();
+    graficarProyectos();
 });
